374ff3f66b7c20b0e62599ca9199aa57699dc2cd
pradeep <solomon.terli@gmail.com>
2017-05-07 19:30:26 +0530

    ur change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,17 +13,17 @@ export class LoginComponent {
    "linkedin":{
      "authEndpoint": this.authServerBaseUrl+"/auth/linkedin",
      "clientId":"8176r44lz2ewos",
-     "redirectURI" : "http://localhost:5000/admin"
+     "redirectURI" : this.authServerBaseUrl+"/admin"
    },
    "facebook":{
      "authEndpoint": this.authServerBaseUrl+"/auth/facebook",
      "clientId":"929055083862567",
-     "redirectURI" : "http://localhost:5000/admin"
+     "redirectURI" : this.authServerBaseUrl+"/admin"
    },
    "google":{
      "authEndpoint": this.authServerBaseUrl+"/auth/google",
      "clientId":"77954512562-eftl8up04q1g3aha2mjg5h6bgel9svkk.apps.googleusercontent.com",
-     "redirectURI" : "http://localhost:5000/admin"
+     "redirectURI" : this.authServerBaseUrl+"/admin"
    }
  };
 }
